601ac8fad8c0ede2cbcb1f013397f34d462f9c09
Rahul Arasikere <arasikere.rahul@gmail.com>
2021-05-10 17:22:57 -0400

    error

--- src/main/target/LINUX/target.c
+++ src/main/target/LINUX/target.c
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <errno.h>
 #include <time.h>
--- src/main/target/LINUX/target.h
+++ src/main/target/LINUX/target.h
@@ -247,6 +247,7 @@ uint64_t millis64_real(void);
 void delayMicroseconds_real(uint32_t us);
 uint64_t micros64(void);
 uint64_t millis64(void);
+uint32_t micros(void);
 
 int lockMainPID(void);
 
