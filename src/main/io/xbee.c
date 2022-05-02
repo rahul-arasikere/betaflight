62a7bb57284b6f5aba736debf0e42557669f88b6
Rahul Arasikere <arasikere.rahul@gmail.com>
2022-05-02 14:10:27 -0400

    xbee ported

--- src/main/io/xbee.c
+++ src/main/io/xbee.c
@@ -1,10 +1,10 @@
 #include <platform.h>
 
-#include "common/time.h"
 #include "common/printf.h"
 #include "common/printf_serial.h"
 
 #include "drivers/serial.h"
+#include "drivers/time.h"
 
 #include "io/serial.h"
 #include "io/xbee.h"
@@ -15,6 +15,16 @@ static serialPort_t *xbeePort = NULL;
 void xbeeInit()
 {
     xbeePort = openSerialPort(XBEE_SERIAL_PORT, FUNCTION_RX_SERIAL, NULL, NULL, BAUD_115200, MODE_RXTX, SERIAL_STOPBITS_1);
+    if (millis() < 10000)
+    {
+        /* delay */
+        delay(10000 - millis());
+    }
     setPrintfSerialPort(xbeePort);
+    tfp_printf(" b\r");
+    delay(300);
+    while(serialRxBytesWaiting(xbeePort)) {
+        serialRead(xbeePort);
+    }
 }
 #endif
